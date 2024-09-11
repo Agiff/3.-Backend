af1ca21e0242c20656932dc2a68c809ff18b9f8a
Agiff <ghifarimohammadrahadian@gmail.com>
2024-09-11 20:34:17 +0700

    first commit

---
+++ 1. npm/src/app.js
@@ -0,0 +1,7 @@
+import "dotenv/config";
+import { userDB, greeting } from "./utils/consts.js";
+
+console.log(greeting);
+console.log(userDB);
+console.log(process.env.jdcSecret);
+console.log(process.env.bikinBaru);
