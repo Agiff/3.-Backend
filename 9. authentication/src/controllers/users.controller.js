05cb931494e2924b3dc358e38e2baddf75a1584f
Agiff <ghifarimohammadrahadian@gmail.com>
2024-10-28 19:40:57 +0700

    feat: add login and hash feature for password on register

--- 9. authentication/src/controllers/users.controller.js
+++ 9. authentication/src/controllers/users.controller.js
@@ -1,25 +1,71 @@
 import { PrismaClient } from "@prisma/client";
+import { comparePassword, hashPassword } from "../utils/bcrypt.js";
+import { generateAccessToken } from "../utils/jwt.js";
 const prisma = new PrismaClient();
 
 class UserController {
-  addUser = async (req, res, next) => {
+  register = async (req, res, next) => {
     try {
       const { username, email, password } = req.body;
 
       if (!username || !email || !password) throw new Error('Bad Request');
 
+      const hashedPassword = await hashPassword(password);
+
       const newUser = await prisma.user.create({
         data: {
           username,
           email,
-          password
+          password: hashedPassword
         }
       })
       
       res.status(201).send(newUser);
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: 'Internal Server Error' });
+      next(error);
+    }
+  }
+
+  login = async (req, res, next) => {
+    try {
+      const { username, email, password } = req.body;
+
+      if (!password || (!email && !username)) throw new Error('Bad Request');
+
+      let user;
+
+      if (email) {
+        user = await prisma.user.findUnique({
+          where: {
+            email
+          }
+        })
+      }
+
+      if (!user && username) {
+        user = await prisma.user.findUnique({
+          where: {
+            username
+          }
+        })
+      }
+
+      if (!user) throw new Error('User not found');
+      const validatedUser = await comparePassword(password, user.password);
+
+      if (!validatedUser) throw new Error('Password is invalid')
+
+      const payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+      }
+
+      const access_token = generateAccessToken(payload);
+      
+      res.send({ access_token });
+    } catch (error) {
+      next(error);
     }
   }
 
@@ -28,8 +74,7 @@ class UserController {
       const users = await prisma.user.findMany();
       res.status(200).send(users);
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: 'Internal Server Error' });
+      next(error);
     }
   }
 
@@ -42,8 +87,7 @@ class UserController {
 
       res.status(200).send(user);
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: 'Internal Server Error' });
+      next(error);
     }
   }
 
@@ -63,8 +107,7 @@ class UserController {
 
       res.status(200).send(user.posts);
     } catch (error) {
-      console.log(error);
-      res.status(500).send({ message: 'Internal Server Error' });
+      next(error);
     }
   }
 }
--- 9. authentication/src/routers/users.routes.js
+++ 9. authentication/src/routers/users.routes.js
@@ -2,9 +2,11 @@ import express from 'express';
 import { userController } from '../controllers/users.controller.js';
 const router = express.Router();
 
-router.post('/', userController.addUser);
 router.get('/', userController.getUsers);
 router.get('/:id', userController.findUser);
 router.get('/:id/posts', userController.getUserPosts);
+
+router.post('/register', userController.register);
+router.post('/login', userController.login);
 
 export default router;
